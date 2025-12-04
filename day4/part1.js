d2b9ed22e8047a129da76e3a58ba83e29da87d88
mirim <mirim920@naver.com>
2025-12-04 21:32:13 +0900

    day4 getIsAccessible 함수 개선

--- day4/part1.js
+++ day4/part1.js
@@ -16,33 +16,28 @@ export const testData = [
 export const getIsAccessible = (data, x, y) => {
   if (data[y][x] === '.') return false;
 
+  const dirs = [
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [-1, 0],
+    [1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+  ];
+
   const xBoundary = data[0].length;
   const yBoundary = data.length;
   let paperCount = 0;
 
-  if (x - 1 >= 0 && y - 1 >= 0) {
-    paperCount += data[y - 1][x - 1] === '@' ? 1 : 0;
-  }
-  if (y - 1 >= 0) {
-    paperCount += data[y - 1][x] === '@' ? 1 : 0;
-  }
-  if (x + 1 < xBoundary && y - 1 >= 0) {
-    paperCount += data[y - 1][x + 1] === '@' ? 1 : 0;
-  }
-  if (x - 1 >= 0) {
-    paperCount += data[y][x - 1] === '@' ? 1 : 0;
-  }
-  if (x + 1 < xBoundary) {
-    paperCount += data[y][x + 1] === '@' ? 1 : 0;
-  }
-  if (x - 1 >= 0 && y + 1 < yBoundary) {
-    paperCount += data[y + 1][x - 1] === '@' ? 1 : 0;
-  }
-  if (y + 1 < yBoundary) {
-    paperCount += data[y + 1][x] === '@' ? 1 : 0;
-  }
-  if (x + 1 < xBoundary && y + 1 < yBoundary) {
-    paperCount += data[y + 1][x + 1] === '@' ? 1 : 0;
+  for (const [dx, dy] of dirs) {
+    const nx = x + dx;
+    const ny = y + dy;
+
+    if (nx >= 0 && nx < xBoundary && ny >= 0 && ny < yBoundary) {
+      paperCount += data[ny][nx] === '@' ? 1 : 0;
+    }
   }
 
   return paperCount < 4;
